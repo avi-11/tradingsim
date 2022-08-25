2ac1c111b71cae345d67af0d00a4fad8b98b9dff
Tejeshwer Singh Sachdeva <sachdevatejeshwersingh@gmail.com>
2022-08-25 17:40:47 +0530

    Trading Sim API Integration

--- src/components/chart/LineChart.js
+++ src/components/chart/LineChart.js
@@ -1,12 +1,25 @@
 import { Line } from "react-chartjs-2";
 
-const LineChart = ({ showGraph, data }) => {
+const LineChart = ({ showGraph, data, labels }) => {
+  const chartData = {
+    labels: [...labels],
+    datasets: [
+      {
+        label: "Simulator",
+        data: [data],
+        fill: false,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+      },
+    ],
+  };
+
   return (
     <div className="chart">
       <label>Metrics</label>
       {showGraph && (
         <div>
-          <Line data={data} width={"500px"} height={"250px"} />
+          <Line data={chartData} width={"500px"} height={"250px"} />
         </div>
       )}
     </div>
